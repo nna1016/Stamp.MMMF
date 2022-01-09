8a5488b2af20e3509d488524141c8b4b6cceeb75
keiji <k019c1084@it-neec.jp>
2022-01-10 02:45:28 +0900

    エラーページ修正、その他微修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,22 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!, except: [:info,:prizelist, :mmmf_terms, :support, :tyusen]
     before_action :configure_permitted_parameters, if: :devise_controller?
     
-    rescue_from ActiveRecord::RecordNotFound, with: :render_404
-    rescue_from ActionController::RoutingError, with: :render_404
-    rescue_from Exception, with: :render_500
-
+    unless Rails.env.development?
+      rescue_from Exception, with: :render_500
+      rescue_from ActiveRecord::RecordNotFound, with: :render_404
+      rescue_from ActionController::RoutingError, with: :render_404
+    end
+   
+    def routing_error
+      raise ActionController::RoutingError, params[:path]
+    end
+   
     def render_404
-      render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
+      render 'errors/404', status: :not_found
     end
     
     def render_500
-      render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
+      render 'errors/500', status: :internal_server_error
     end
 
     protected
