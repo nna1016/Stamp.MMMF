04e848cf4bd3bcbe87d373fbe1dcf1553c049bde
keiji <k019c1084@it-neec.jp>
2022-01-12 14:59:06 +0900

    homeにスタンプが表示されるようにした

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,2 +1,17 @@
 module HomesHelper
+    def stamp_list(index)
+        path = "https://pbs.twimg.com/profile_images/1022063592441446400/_truTOvX_400x400.jpg"
+        unless @get_stamp.nil?
+            @get_stamp.each do |get_stamp|
+                if get_stamp.number == index
+                    begin
+                        path = @stamp.find(index).image.url
+                    rescue
+                        path = "/images/noimage.png"
+                    end
+                end
+            end
+        end
+        return path
+    end
 end
