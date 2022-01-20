d9fbfc4acd8f72a713bbaaefab8c8f8838019a7f
keiji <k019c1084@it-neec.jp>
2022-01-20 11:18:33 +0900

    スタンプ獲得機能追加、利用規約のレイアウト修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,10 @@ class HomesController < ApplicationController
   before_action :access_log
   
   def index
+    if GetStamp.find_by(student_no: current_user.student_no, stamp_id: "act").nil?
+      GetStamp.create({student_no: current_user.student_no, number: 0, stamp_id: "act"})
+      flash[:notice] = '登録ボーナスのスタンプGET！'
+    end
     @get_stamp = GetStamp.where(student_no: current_user.student_no)
     @stamp = Stamp.all
     @prize = Prize.find_by(student_no: current_user.student_no)
