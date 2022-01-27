8120226b7155b40baed20c6a10a401841d672f96
keiji <k019c1084@it-neec.jp>
2022-01-27 17:38:21 +0900

    スタンプカードの裏面に抽選参加用QR作成、抽選参加ページ削除

--- app/helpers/tyusens_helper.rb
+++ app/helpers/tyusens_helper.rb
@@ -1,17 +1,2 @@
 module TyusensHelper
-    require 'chunky_png'
-
-    def qrcode_tag(url, _options = {})
-        qr = ::RQRCode::QRCode.new(url)
-        ChunkyPNG::Image.from_datastream(qr.as_png.resize(200,200).to_datastream).to_data_url
-    end
-
-    # ざわつかせる
-    def zawa 
-        if rand(1..10) >= 8
-            true
-        else  
-            false        
-        end
-    end
 end
