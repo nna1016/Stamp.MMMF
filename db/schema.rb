343cdaaad54f6c637111a1e2bd5a07bdc9c7df47
’Nagi’ <nagi.ishizuka@zozo.com>
2022-01-31 22:54:03 +0900

    バナー広告の変更

--- db/schema.rb
+++ db/schema.rb
@@ -21,12 +21,12 @@ ActiveRecord::Schema.define(version: 2022_01_23_035241) do
   end
 
   create_table "prizes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "student_no"
     t.string "prize"
-    t.integer "qty"
-    t.string "category"
-    t.string "image"
+    t.string "reg_user"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["student_no"], name: "index_prizes_on_student_no", unique: true
   end
 
   create_table "site_infos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
