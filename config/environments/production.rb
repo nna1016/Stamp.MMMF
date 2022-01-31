d98a59decf5287602869967429c146d5aa57ea4e
’Nagi’ <nagi.ishizuka@zozo.com>
2022-01-31 14:45:28 +0900

    host変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   
-  config.action_mailer.default_url_options = { protocol: 'https', host: 'www.mmmf.jp'}
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'stamp.mmmf.jp'}
 
   config.action_mailer.perform_deliveries = true
 
