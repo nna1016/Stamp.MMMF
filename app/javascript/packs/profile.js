462193fed0aaba639dba2d79d9284d51060fa087
’Nagi’ <nagi.ishizuka@zozo.com>
2022-01-31 19:10:06 +0900

    js修正

--- app/javascript/packs/profile.js
+++ app/javascript/packs/profile.js
@@ -1,10 +1,8 @@
 document.addEventListener('turbolinks:load', () => {
-  $(function () {
     function profile() {
       $('.stamp-block').on('click', function () {
         $('.stamp-item').toggleClass('flipped');
       });
     };
     profile();
-  });
 });
