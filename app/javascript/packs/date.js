940d950b9e9d5b02c5fba388b6866b32fe66899c
nna1016 <n.ishizuka@becalm.jp>
2022-02-07 10:20:06 +0900

    入退場管理ページの読み込み処理修正

--- app/javascript/packs/date.js
+++ app/javascript/packs/date.js
@@ -1,21 +1,5 @@
 document.addEventListener('turbolinks:load', () => {
-    function twoDigit(num) {
-        let ret;
-        if (num < 10)
-            ret = "0" + num;
-        else
-            ret = num;
-        return ret;
-    }
-    function showClock() {
-        let nowTime = new Date();
-        let nowHour = twoDigit(nowTime.getHours());
-        let nowMin = twoDigit(nowTime.getMinutes());
-        let nowSec = twoDigit(nowTime.getSeconds());
-        let msg = nowHour + ":" + nowMin + ":" + nowSec;
-        document.getElementById("realtime").innerHTML = msg;
-    }
-    setInterval('showClock()', 1000);
+
 
     function date() {
         mydate = new Date();
